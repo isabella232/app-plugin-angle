15763d3945b20eaa8502e799d8ecc91a18a1cea1
picodes <picodes@angle.money>
2021-12-20 15:20:31 +0100

    Add stable mint/burn tests

---
+++ tests/src/burn_stable.test.js
@@ -0,0 +1,89 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import { waitForAppScreen, zemu, genericTx, SPECULOS_ADDRESS, RANDOM_ADDRESS, txFromEtherscan} from './test.fixture';
+import { ethers } from "ethers";
+import { parseEther, parseUnits} from "ethers/lib/utils";
+
+// Angle Protocol: Stable Master Front (proxy)
+const contractAddr = "0x5addc89785d75c86ab939e9e15bfbbb7fc086a87";
+const pluginName = "angle";
+const abi_path = `../${pluginName}/abis/` + contractAddr + '.json';
+const abi = require(abi_path);
+
+
+// Reference transaction for this test:
+// https://etherscan.io/tx/0x6940602f95e82d3c68903e001783f20bd145176831033b66da9373d39d3cd876
+
+test('[Nano S] Burn agEUR for USDC with sender != burner and dest', zemu("nanos", async (sim, eth) => {
+
+  const serializedTx = txFromEtherscan("0x02f90113018201708459682f00851e3a4dc5ee8303cb9c945addc89785d75c86ab939e9e15bfbbb7fc086a8780b8a46d1042160000000000000000000000000000000000000000000009247656c80b2129e79600000000000000000000000029864b53c8d7509de2ccf5fb76488cfd102ddfd000000000000000000000000029864b53c8d7509de2ccf5fb76488cfd102ddfd0000000000000000000000000e9f183fc656656f1f17af1f2b0df79b8ff9ad8ed0000000000000000000000000000000000000000000000000000000b2d884b41c080a0fee84d44824dc9628bde5e90b4954f41a5f93a02906503dd23739e3ffb7e5022a03455c4c56c169ad9859f4c2a79f06cf2850eb9442e10a185d77f2548009d65d2");
+
+  const tx = eth.signTransaction(
+    "44'/60'/0'/0",
+    serializedTx,
+  );
+
+  // Wait for the application to actually load and parse the transaction
+  await waitForAppScreen(sim);
+  // Navigate the display by pressing the right button 10 times, then pressing both buttons to accept the transaction.
+  // EDIT THIS: modify `10` to fix the number of screens you are expecting to navigate through.
+  await sim.navigateAndCompareSnapshots('.', 'nanos_burn_ageur_usdc_with_different_burner_and_dest', [14, 0]);
+
+  await tx;
+}));
+
+
+test('[Nano S] Burn agEUR with USDC with sender == burner and dest', zemu("nanos", async (sim, eth) => {
+  const contract = new ethers.Contract(contractAddr, abi);
+
+  // Constants used to create the transaction
+  // EDIT THIS: Remove what you don't need
+  const amount = parseUnits("43173908355374949984150", 'wei');
+  const burner = SPECULOS_ADDRESS;
+  const dest = SPECULOS_ADDRESS;
+  const poolManager = "0xe9f183fc656656f1f17af1f2b0df79b8ff9ad8ed";
+  const minCollatAmount = parseUnits("48008547137", 'wei');
+
+  const {data} = await contract.populateTransaction.burn(amount, burner, dest, poolManager, minCollatAmount);
+
+  // Get the generic transaction template
+  let unsignedTx = genericTx;
+  // Modify `to` to make it interact with the contract
+  unsignedTx.to = contractAddr;
+  // Modify the attached data
+  unsignedTx.data = data;
+
+  // Create serializedTx and remove the "0x" prefix
+  const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
+
+  const tx = eth.signTransaction(
+    "44'/60'/0'/0",
+    serializedTx
+  );
+
+  // Wait for the application to actually load and parse the transaction
+  await waitForAppScreen(sim);
+  // Navigate the display by pressing the right button 10 times, then pressing both buttons to accept the transaction.
+  // EDIT THIS: modify `10` to fix the number of screens you are expecting to navigate through.
+  await sim.navigateAndCompareSnapshots('.', 'nanos_burn_ageur_usdc_with_equal_sender_burner_and_dest', [7, 0]);
+
+  await tx;
+}));
+
+// NanoX test (same as nano s, except we use a raw tx instead of rebuilding one from contract abi)
+test('[Nano X] Burn agEUR with USDC with sender != user', zemu("nanox", async (sim, eth) => {
+
+  const serializedTx = txFromEtherscan("0x02f90113018201708459682f00851e3a4dc5ee8303cb9c945addc89785d75c86ab939e9e15bfbbb7fc086a8780b8a46d1042160000000000000000000000000000000000000000000009247656c80b2129e79600000000000000000000000029864b53c8d7509de2ccf5fb76488cfd102ddfd000000000000000000000000029864b53c8d7509de2ccf5fb76488cfd102ddfd0000000000000000000000000e9f183fc656656f1f17af1f2b0df79b8ff9ad8ed0000000000000000000000000000000000000000000000000000000b2d884b41c080a0fee84d44824dc9628bde5e90b4954f41a5f93a02906503dd23739e3ffb7e5022a03455c4c56c169ad9859f4c2a79f06cf2850eb9442e10a185d77f2548009d65d2");
+
+  const tx = eth.signTransaction(
+    "44'/60'/0'/0",
+    serializedTx,
+  );
+
+  // Wait for the application to actually load and parse the transaction
+  await waitForAppScreen(sim);
+  // Navigate the display by pressing the right button 6 times, then pressing both buttons to accept the transaction.
+  await sim.navigateAndCompareSnapshots('.', 'nanox_burn_ageur_usdc_with_different_burner_and_dest', [7, 0]);
+
+  await tx;
+}));
---
+++ tests/src/mint_stable.test.js
@@ -0,0 +1,106 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import { waitForAppScreen, zemu, genericTx, SPECULOS_ADDRESS, RANDOM_ADDRESS, txFromEtherscan} from './test.fixture';
+import { ethers } from "ethers";
+import { parseEther, parseUnits} from "ethers/lib/utils";
+
+// Angle Protocol: Stable Master Front (proxy)
+const contractAddr = "0x5addc89785d75c86ab939e9e15bfbbb7fc086a87";
+const pluginName = "angle";
+const abi_path = `../${pluginName}/abis/` + contractAddr + '.json';
+const abi = require(abi_path);
+
+
+// Reference transaction for this test:
+// https://etherscan.io/tx/0x89a319da4fe385d075d0fdeaee28b56012e3861ea72bbebea82190538c65abb6
+
+test('[Nano S] Mint agEUR with USDC with sender != user', zemu("nanos", async (sim, eth) => {
+  const contract = new ethers.Contract(contractAddr, abi);
+
+  // Constants used to create the transaction
+  // EDIT THIS: Remove what you don't need
+  const amount = parseUnits("25213382881", 'wei');
+  const user = RANDOM_ADDRESS;
+  const poolManager = "0xe9f183fc656656f1f17af1f2b0df79b8ff9ad8ed";
+  const minStableAmount = parseUnits("22069962301657650640274", 'wei');
+
+  const {data} = await contract.populateTransaction.mint(amount, user, poolManager, minStableAmount);
+
+  // Get the generic transaction template
+  let unsignedTx = genericTx;
+  // Modify `to` to make it interact with the contract
+  unsignedTx.to = contractAddr;
+  // Modify the attached data
+  unsignedTx.data = data;
+
+  // Create serializedTx and remove the "0x" prefix
+  const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
+
+  const tx = eth.signTransaction(
+    "44'/60'/0'/0",
+    serializedTx
+  );
+
+  // Wait for the application to actually load and parse the transaction
+  await waitForAppScreen(sim);
+  // Navigate the display by pressing the right button 10 times, then pressing both buttons to accept the transaction.
+  // EDIT THIS: modify `10` to fix the number of screens you are expecting to navigate through.
+  await sim.navigateAndCompareSnapshots('.', 'nanos_mint_ageur_usdc_with_different_sender_and_user', [10, 0]);
+
+  await tx;
+}));
+
+
+test('[Nano S] Mint agEUR with USDC with sender == user', zemu("nanos", async (sim, eth) => {
+  const contract = new ethers.Contract(contractAddr, abi);
+
+  // Constants used to create the transaction
+  // EDIT THIS: Remove what you don't need
+  const amount = parseUnits("25213382881", 'wei');
+  const user = SPECULOS_ADDRESS;
+  const poolManager = "0xe9f183fc656656f1f17af1f2b0df79b8ff9ad8ed";
+  const minStableAmount = parseUnits("22069962301657650640274", 'wei');
+
+  const {data} = await contract.populateTransaction.mint(amount, user, poolManager, minStableAmount);
+
+  // Get the generic transaction template
+  let unsignedTx = genericTx;
+  // Modify `to` to make it interact with the contract
+  unsignedTx.to = contractAddr;
+  // Modify the attached data
+  unsignedTx.data = data;
+
+  // Create serializedTx and remove the "0x" prefix
+  const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
+
+  const tx = eth.signTransaction(
+    "44'/60'/0'/0",
+    serializedTx
+  );
+
+  // Wait for the application to actually load and parse the transaction
+  await waitForAppScreen(sim);
+  // Navigate the display by pressing the right button 10 times, then pressing both buttons to accept the transaction.
+  // EDIT THIS: modify `10` to fix the number of screens you are expecting to navigate through.
+  await sim.navigateAndCompareSnapshots('.', 'nanos_mint_ageur_usdc_with_equal_sender_and_user', [7, 0]);
+
+  await tx;
+}));
+
+// NanoX test (same as nano s, except we use a raw tx instead of rebuilding one from contract abi)
+test('[Nano X] Mint agEUR with USDC with sender != user', zemu("nanox", async (sim, eth) => {
+
+  const serializedTx = txFromEtherscan("0x02f8f3018203f8843b9aca00850cab28e0bc8303c90e945addc89785d75c86ab939e9e15bfbbb7fc086a8780b884074ee44600000000000000000000000000000000000000000000000000000005ded5b0e10000000000000000000000006cd71d6cb7824add7c277f2ca99635d98f8b9248000000000000000000000000e9f183fc656656f1f17af1f2b0df79b8ff9ad8ed0000000000000000000000000000000000000000000004ac6a40f5af96bd3592c080a069fdc24b39343a2db06a432b9185d73eaffcbe7fc2be4bdf3e9d6ef3f32ddc91a056efe6f205331afd61c5dc99e591d4ddc5ab47e0a479a57519c5bc8762676da0");
+
+  const tx = eth.signTransaction(
+    "44'/60'/0'/0",
+    serializedTx,
+  );
+
+  // Wait for the application to actually load and parse the transaction
+  await waitForAppScreen(sim);
+  // Navigate the display by pressing the right button 6 times, then pressing both buttons to accept the transaction.
+  await sim.navigateAndCompareSnapshots('.', 'nanox_mint_ageur_usdc_with_different_sender_and_user', [6, 0]);
+
+  await tx;
+}));
